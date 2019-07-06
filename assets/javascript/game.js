ee3ef1c943a1d2b709e5fad6462cb405f3af087c
Katie Rawert <kmrawert@yahoo.com>
2019-07-06 08:43:32 -0500

    added psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//Crystal Game - Pseudo Code
+// Create/clone Git Repository
+// Set up folder structure - Assets (CSS, JS, and Images), CSS - Reset/style; Git Push
+// Add 4 crystals to the images folder, and html page
+// Must have an on-click event for the crystals to be interactive (Console.log)
+// Create random number generator and print number to page
+// Associate/Assign a value to each Crystal image. (add a val=1-12 in HTML?)
+// Set up addition fuctionality (var + var?)
+// Set up total fuctionality or score counter? 
+// Set up if statement 
+    // if total is greater than random number, then you lose
+        // alert you lose! losses++ (console log)
+        // reset game
+    // if total is less than random number, allow more guesses/nothing
+    // if total matches random number, then you win
+        // aleart you win! wins++ (console log)
+        // reset game
+// Set up reset function (similar to the letter guess game)
+
+
+
+// Potential variables = Wins, Losses, ComputerGuessedNum, Total, YourGuesses, Reset
